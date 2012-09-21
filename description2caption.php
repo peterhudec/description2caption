70a80b846e38ad464d538a34c61c9cb3585fd272
Peter Hudec <peterhudec@peterhudec.com>
2012-09-21 15:50:01 +0200

    Reorganized the whole code into a single class to avoid poluting the global namespace.

--- description2caption.php
+++ description2caption.php
@@ -9,101 +9,135 @@ Plugin URI: http://peterhudec.com/programming/description2caption
 License: GPL2
 */
 
-// hooks
-register_activation_hook( __FILE__, 'd2c_defaults' );
-add_filter('plugin_action_links', 'd2c_action_links', 10, 2);
-add_action('add_attachment', 'd2c');
-add_action('admin_menu', 'd2c_options');
-add_action('admin_init', 'd2c_init');
-
-
-//add setting link to plugins list
-function d2c_action_links($links, $file) {
-    static $this_plugin;
-    if (!$this_plugin) {
-        $this_plugin = plugin_basename(__FILE__);
-    }
-    if ($file == $this_plugin) {
-        $settings_link = '<a href="' . get_bloginfo('wpurl') . '/wp-admin/admin.php?page=d2c-options">Settings</a>';
-        array_unshift($links, $settings_link);
-    }
-    return $links;
-}
-
-function d2c_defaults(){
-	add_option('d2c_move', 0);
-}
-
 /**
- * The "Caption" input field in the media item form is filled with
- * post excerpt in the get_attachment_fields_to_edit() function
- * 
- * This action copies the attachment's post_content to its post_excerpt
- * after the attachment post is inserted to the DB after file upload
+ * Main plugin class
+ * the long name is intentional to avoid collisions in global namespace
  */
-function d2c($post_ID){
-	$post = get_post($post_ID);
-	$post->post_excerpt = $post->post_content;
-	if(get_option('d2c_move')) $post->post_content = null;
-	wp_update_post( $post );
-}
-
-/**
- * Options
- */
-
-// add plugin settings page
-function d2c_options(){
-	add_plugins_page(
-		'Description 2 Caption',
-		'Description 2 Caption',
-		'administrator',
-		'd2c-options',
-		'd2c_options_cb');
-}
-
-// add settings
-function d2c_init(){
-	register_setting(
-		'd2c_move',
-		'd2c_move' );
-	add_settings_section(
-		'd2c_section',
-		'',
-		'd2c_section_cb',
-		'd2c-options');
-	add_settings_field(
-		'd2c_move',
-		'Copy or move caption to description?',
-		'd2c_move_cb',
-		'd2c-options',
-		'd2c_section');
-}
-function d2c_section_cb(){}
-
-// options page form
-function d2c_options_cb(){
-	?>
+class Description_2_Caption_Plugin {
+	
+    /**
+     * Constructor
+     */
+	function __construct() {
+	    // hooks
+		register_activation_hook( __FILE__, array( $this, 'register' ) );
+        
+        // filters
+        add_filter( 'plugin_action_links', array( $this, 'plugin_action_links' ), 10, 2 );
+        
+        // actions
+        add_action( 'add_attachment', array( $this, 'add_attachment' ) );
+        add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+        add_action( 'admin_init', array( $this, 'init' ) );
+	}
+    
+    /**
+     * Default settings
+     */
+    public function register() {
+        add_option( 'description_2_caption_move', 0 );
+    }
+    
+    /**
+     * Plugin action links
+     */
+    public function plugin_action_links( $links, $file ) {
+        static $this_plugin;
+        if ( ! $this_plugin ) {
+            $this_plugin = plugin_basename( __FILE__ );
+        }
+        if ( $file == $this_plugin ) {
+            $settings_link = '<a href="' . get_bloginfo( 'wpurl' ) . '/wp-admin/admin.php?page=d2c-options">Settings</a>';
+            array_unshift( $links, $settings_link );
+        }
+        return $links;
+    }
+    
+    /**
+     * Trigered after user uploads attachment and before the attachment form gets displayed
+     */
+    public function add_attachment( $post_ID )
+    {
+        $post = get_post( $post_ID );
+        $post->post_excerpt = $post->post_content;
+        if ( get_option( 'description_2_caption_move') ) {
+        	$post->post_content = null;
+		}
+        wp_update_post( $post );
+    }
+    
+    /**
+     * Adds plugin entry in the admin menu
+     */
+    public function admin_menu() {
+        add_plugins_page(
+	        'Description 2 Caption',
+	        'Description 2 Caption',
+	        'administrator',
+	        'd2c-options',
+	        array( $this, 'options_cb' )
+		);
+    }
+    
+    /**
+     * Called upon plugin initialization
+     */
+    public function init()
+    {
+        register_setting(
+            'description_2_caption_move',
+            'description_2_caption_move'
+		);
+        add_settings_section(
+            'd2c_section',
+            '',
+            array( $this, 'section_cb' ),
+            'd2c-options'
+		);
+        add_settings_field(
+            'description_2_caption_move',
+            'Copy or move caption to description?',
+            array( $this, 'move_cb' ),
+            'd2c-options',
+            'd2c_section'
+		);
+    }
+    
+    /**
+     * Empty section callback
+     */
+    public function section_cb() {}
+    
+	/**
+	 * Options callback
+	 */
+    public function options_cb() { ?>
 	<div class="wrap">
 		<h2>Description 2 Caption options</h2>
 		<?php settings_errors(); ?>
 		<form action="options.php" method="post">
-			<?php settings_fields('d2c_move'); ?>
-			<?php do_settings_sections('d2c-options'); ?>
+			<?php settings_fields( 'description_2_caption_move' ); ?>
+			<?php do_settings_sections( 'd2c-options' ); ?>
 			<?php submit_button(); ?>
 		</form>
-		<p>Created by <strong>Peter Hudec</strong>, <a href="http://peterhudec.com" target="_blank">peterhudec.com</a>.</p>
+		<p>
+			Created by <strong>Peter Hudec</strong>, <a href="http://peterhudec.com" target="_blank">peterhudec.com</a>.
+		</p>
 	</div>
-	<?php
+	<?php }
+	
+	/**
+	 * Move setting callback
+	 */
+	public function move_cb() { ?>
+		<input type="radio" id="description_2_caption_copy" name="description_2_caption_move" value="0" <?php checked( 0, get_option( 'description_2_caption_move' ) ); ?> />
+		<label for="d2c_copy">Copy</label>
+		<br />
+		<input type="radio" id="description_2_caption_move" name="description_2_caption_move" value="1" <?php checked( 1, get_option( 'description_2_caption_move' ) ); ?> />
+		<label for="description_2_caption_move">Move</label>
+	<?php }
 }
 
-// copy or move setting content
-function d2c_move_cb(){
-	?>
-	<input type="radio" id="d2c_copy" name="d2c_move" value="0" <?php checked(0, get_option('d2c_move')); ?> />
-	<label for="d2c_copy">Copy</label><br />
-	<input type="radio" id="d2c_move" name="d2c_move" value="1" <?php checked(1, get_option('d2c_move')); ?> />
-	<label for="d2c_move">Move</label>
-	<?php
-}
+// instantiate plugin with long name to avoid collisions with other plugins
+$description_2_caption_plugin = new Description_2_Caption_Plugin();
 ?>
